7dd7b51371082f509dce82283ebc31a966b9844a
Drew Taylor <drew@infiniteinternet.ca>
2021-08-29 15:00:31 -0400

    fix relative path

--- dist/assets/js/e16015ca.c5514cfb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkarchway_docs=self.webpackChunkarchway_docs||[]).push([[700],{5688:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
