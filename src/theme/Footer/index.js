754f0192797240d515af66a624524bc46efc79e9
Drew Taylor <drew@infiniteinternet.ca>
2021-10-08 10:03:59 -0400

    Styles

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,16 +1,10 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import React from 'react';
-import clsx from 'clsx';
+// import clsx from 'clsx';
+// import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
 import {useThemeConfig} from '@docusaurus/theme-common';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import isInternalUrl from '@docusaurus/isInternalUrl';
-import styles from './styles.module.css';
 import ThemedImage from '@theme/ThemedImage';
 import IconExternalLink from '@theme/IconExternalLink';
 
