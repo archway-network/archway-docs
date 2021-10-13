9727d0c4110f0198d226aacb302c3d8cc069f024
Drew Taylor <drew@infiniteinternet.ca>
2021-10-13 10:38:29 -0400

    Remove legacy htpasswd and semantic text changes to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/create/getting-started/install">
-            Setup Tutorial - 5min ⏱️
+            Setup Tutorial - 5 min ⏱️
           </Link>
         </div>
       </div>
