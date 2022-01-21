f1fceb7af25c45eac34b7e1b49421c05c857f5b5
Drew Taylor <drew@infiniteinternet.ca>
2022-01-21 12:26:09 -0500

    Docs pages nav pills

---
+++ src/pages/participate.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import styles from './participate.module.css';
+
+export default function Participate() {
+  const {siteConfig} = useDocusaurusContext();
+  const categories = siteConfig.customFields.pageCategories;
+  
+  let participateCats = categories[2].children;
+
+  const categoryItems = (participateCats.length) ? participateCats.map((item,i) =>
+    <div className='cat-item' key={i}>
+      <p>
+        <a href={item.value}>{item.name}</a>
+      </p>
+    </div>
+  ) : null;
+
+
+  return (
+    <Layout title="Participate" description="Your launchpad into Cosmos">
+      <main className='page'>
+        <section className={styles.feature}>
+          <div className={styles.featurePrimary}>
+            <div className={clsx('hero hero--primary', styles.heroBanner)}>
+              <div className="container">
+                <h1>Participate</h1>
+              </div>
+              <div className='container-fluid cat-wr'>{categoryItems}</div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
