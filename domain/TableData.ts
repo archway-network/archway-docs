42d7a5f0cf3dcc8e44c19e8564599e3dfdccec05
OsamaBinAsim <osama.asim3@gmail.com>
2023-01-31 01:10:58 +0500

    address comment to update type

--- domain/TableData.ts
+++ domain/TableData.ts
@@ -3,7 +3,7 @@ import { HeaderItem, TableRow } from '@/types';
 class TableData {
   constructor(public header: HeaderItem[], public rows: TableRow[]) {}
 
-  static make(attributes: any) {
+  static make(attributes: { header: HeaderItem[]; rows: TableRow[] }) {
     return new TableData(attributes?.header || [{}], attributes?.rows || [{}]);
   }
 }
