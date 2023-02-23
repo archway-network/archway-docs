ae93ef44ba25583faf5a864a69d46693da95419a
OsamaBinAsim <osama.asim3@gmail.com>
2023-02-23 21:50:34 +0500

    changes related to table and icon

--- components/Ui/Icons/CopyIcon.vue
+++ components/Ui/Icons/CopyIcon.vue
@@ -2,14 +2,14 @@
   <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M26 15H17C15.8954 15 15 15.8954 15 17V26C15 27.1046 15.8954 28 17 28H26C27.1046 28 28 27.1046 28 26V17C28 15.8954 27.1046 15 26 15Z"
-      stroke="black"
+      stroke="currentColor"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M11 21H10C9.46957 21 8.96086 20.7893 8.58579 20.4142C8.21071 20.0391 8 19.5304 8 19V10C8 9.46957 8.21071 8.96086 8.58579 8.58579C8.96086 8.21071 9.46957 8 10 8H19C19.5304 8 20.0391 8.21071 20.4142 8.58579C20.7893 8.96086 21 9.46957 21 10V11"
-      stroke="black"
+      stroke="currentColor"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
