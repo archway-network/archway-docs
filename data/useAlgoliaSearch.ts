0fbb62eb54ed4119fea6cc59363699972f30188f
jsoneaday <1474935+jsoneaday@users.noreply.github.com>
2023-03-06 11:39:56 -0500

    remove rank switching replace by switching index (#178)

--- data/useAlgoliaSearch.ts
+++ data/useAlgoliaSearch.ts
@@ -2,13 +2,13 @@ import { AlgoliaSearch } from '@/domain';
 import { useRuntimeConfig } from '#app';
 import { SortingReplicas } from '~~/domain/AlgoliaSearch';
 
-export const useAlgoliaSearch: () => Promise<{
+export const useAlgoliaSearch: (sortingReplica?: SortingReplicas) => Promise<{
   findObject: (searchPredicate: (hit: any) => boolean) => Promise<any>,
   updateObjectsPartially: (objs: any[], createIfNotExists?: boolean) => Promise<any>,
-  search: (query: string, sortingReplica: SortingReplicas, filters?: string) => Promise<any>
-}> = async () => {
+  search: (query: string, filters?: string) => Promise<any>
+}> = async (sortingReplica?: SortingReplicas) => {
     const runtimeConfig = useRuntimeConfig();
-    const algoliaSearch = new AlgoliaSearch(runtimeConfig.algolia.appId, runtimeConfig.algolia.writeApiKey, runtimeConfig.algolia.docIndex, runtimeConfig.algolia.env);
+    const algoliaSearch = new AlgoliaSearch(runtimeConfig.algolia.appId, runtimeConfig.algolia.writeApiKey, runtimeConfig.algolia.docIndex, runtimeConfig.algolia.env, sortingReplica);
 
     const findObject = async (searchPredicate: (hit: any) => boolean) => {
         return await algoliaSearch.findObject(searchPredicate);
@@ -18,8 +18,8 @@ export const useAlgoliaSearch: () => Promise<{
         return await algoliaSearch.updateObjectsPartially(objs, createIfNotExists);
     }
 
-    const search = async (query: string, sortingReplica: SortingReplicas, filters?: string) => {
-        return await algoliaSearch.search(query, sortingReplica, filters);
+    const search = async (query: string, filters?: string) => {
+        return await algoliaSearch.search(query, filters);
     }
 
   return { findObject, updateObjectsPartially, search };
