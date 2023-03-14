b3d5c7f62995cbfdb55c2198a63353beb5ec7058
Marius Petroaie <marius.petroaie@gmail.com>
2023-03-14 14:20:48 +0200

    add the ability to run the website without the WRITE_API_KEY being present

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,5 +1,6 @@
 import { serverQueryContent } from '#content/server';
 import { SitemapStream, streamToPromise } from 'sitemap';
+
 export default defineEventHandler(async event => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find();
