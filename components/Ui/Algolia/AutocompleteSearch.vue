478d9ad3367f93dd595c9577a7eae4f4be1a3fdd
Marius Petroaie <marius.petroaie@gmail.com>
2023-03-29 13:34:29 +0300

    fix search results not being clickable

--- components/Ui/Algolia/AutocompleteSearch.vue
+++ components/Ui/Algolia/AutocompleteSearch.vue
@@ -8,18 +8,33 @@
     componentId: { type: String, default: 'autocomplete' },
     detached: { type: Boolean, default: false },
   });
+
   const { algolia } = useRuntimeConfig();
-  const algoliaRef = algolia.appId === 'mock' ? undefined : algoliasearch(algolia.appId, algolia.searchApiKey, { headers: { 'x-algolia-application-id': algolia.appId } });
-  const articles = ref<ParsedContent[]|undefined>()
+
+  const algoliaRef =
+    algolia.appId === 'mock'
+      ? undefined
+      : algoliasearch(algolia.appId, algolia.searchApiKey, { headers: { 'x-algolia-application-id': algolia.appId } });
+
+  const articles = ref<ParsedContent[] | undefined>();
+
   onMounted(() => {
     (async () => {
-    articles.value = await queryContent('/').find()
+      articles.value = await queryContent('/').find();
     })();
-  })
+  });
+
+  const hasArticles = computed(() => (articles.value || [])?.length > 0);
 </script>
 
 <template>
-  <div>
-    <AutocompleteInput :componentId="componentId" :detached="detached" :indexName="algolia.docIndex" :algoliaRef="algoliaRef" :articles="articles" />
+  <div v-if="hasArticles">
+    <AutocompleteInput
+      :componentId="componentId"
+      :detached="detached"
+      :indexName="algolia.docIndex"
+      :algoliaRef="algoliaRef"
+      :articles="articles"
+    />
   </div>
 </template>
