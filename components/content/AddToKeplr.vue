d362ca7d262ee17a5cdcd6d8de64063f412eda11
eliasmpw <eliasmpw@hotmail.com>
2023-11-10 19:19:11 +0100

    feat: create button that adds archway to keplr when clicked

---
+++ components/content/AddToKeplr.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+  import { PrimaryButton } from '@/components/Ui';
+
+  const currency = {
+    coinDenom: 'ARCH',
+    coinMinimalDenom: 'aarch',
+    coinDecimals: 18,
+    coinGeckoId: 'archway',
+    coinImageUrl: 'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/archway/aarch.png',
+  };
+
+  const archwayChainInfo = {
+    bech32Config: {
+      bech32PrefixAccAddr: 'archway',
+      bech32PrefixAccPub: 'archwaypub',
+      bech32PrefixConsAddr: 'archwayvalcons',
+      bech32PrefixConsPub: 'archwayvalconspub',
+      bech32PrefixValAddr: 'archwayvaloper',
+      bech32PrefixValPub: 'archwayvaloperpub',
+    },
+    bip44: {
+      coinType: 118,
+    },
+    chainId: 'archway-1',
+    chainName: 'Archway',
+    chainSymbolImageUrl: 'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/archway/chain.png',
+    currencies: [currency],
+    features: ['cosmwasm'],
+    feeCurrencies: [
+      {
+        ...currency,
+        gasPriceStep: {
+          low: 1000000000000,
+          average: 1500000000000,
+          high: 2000000000000,
+        },
+      },
+    ],
+    rest: 'https://api.mainnet.archway.io',
+    rpc: 'https://rpc.mainnet.archway.io',
+    stakeCurrency: currency,
+    nodeProvider: {
+      name: 'Phi Labs',
+      email: 'support@philabs.xyz',
+      website: 'https://philabs.xyz',
+    },
+  };
+
+  const addToKeplr = async () => {
+    await window.keplr.experimentalSuggestChain(archwayChainInfo);
+  };
+</script>
+
+<template>
+  <PrimaryButton @click="() => addToKeplr()">
+    <slot></slot>
+  </PrimaryButton>
+</template>
