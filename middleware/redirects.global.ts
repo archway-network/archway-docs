b07f014599d6bd2f4ad2e0705520b80de53e7414
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2024-07-25 17:28:56 -0500

    Revamp developer docs structure (#502)
    * Updated menu title, page title, and meta description for the Getting Started -> Installation page
    
    * Updated menu title for the Setup page under Getting Started
    
    * Addition of an introduction page to Developer Getting Started section
    
    * Addition of the compile contract page to Developer Getting Started section
    
    * Reorder the understanding gas fees page
    
    * Add a note that directly utilizing the rust optimizer works for both contracts built using the Archway Devloper CLI and other contracts
    
    * Removal of deleted page in understanding gas fees which had a name change for the reording done previously
    
    * Addition of example output for compiling a contract
    
    * Addition of the deploy and instantiate page to Developer Getting Started section
    
    * Addition of the interacting with your contract page to Developer Getting Started section
    
    * Update to the menu title for the understanding gas and fees page
    
    * Remove the Getting Started introduction page from the CosmWasm Documentation section and add a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Addition of a network access section to the getting started introduction page taken from the getting started introduction page from the cosmwasm documentation section. Also modifications to the area-52 section.
    
    * Remove the Getting Started setting up environment page from the CosmWasm Documentation section and added a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Remove the Getting Started compile contract page from the CosmWasm Documentation section and added a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Remove the Getting Started unit testing page from the CosmWasm Documentation section, added that page's content to the smart contracts testing page in the cosmwasm documentation section and added a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Remove the Getting Started deploy and interact with contract page from the CosmWasm Documentation section and added a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Remove the Getting Started integration overview page from the CosmWasm Documentation section and moved it to the smart contracts section and also added a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Remove the Getting Started next steps page from the CosmWasm Documentation section and added a 304 redirect in case search engines or others have indexed or referenced the page
    
    * Restructuring of the Cosmwasm Documentation docs into the Smart Contracts improved folder structure
    
    * Moved the cosmwasm by example tutorials to the Guides section and set the necessary redirects
    
    * Move the storage tutorials to the Guides section
    
    * Removal of the simple option tutorial

--- middleware/redirects.global.ts
+++ middleware/redirects.global.ts
@@ -5,6 +5,58 @@ const redirects: Record<string, string> = {
   '/validators/requirements': '/validators/becoming-a-validator/requirements',
   '/community/wallet-setup/keplr-setup': '/community/wallet-setup/keplr/keplr-setup',
   '/community/airdrop-snapshots': '/community/dropcamp',
+  '/developers/cosmwasm-documentation/getting-started/introduction': '/developers/getting-started/introduction',
+  '/developers/cosmwasm-documentation/getting-started/setting-up-environment': '/developers/getting-started/install',
+  '/developers/cosmwasm-documentation/getting-started/compile-contract': '/developers/getting-started/compile-contract',
+  '/developers/cosmwasm-documentation/getting-started/unit-tests': '/developers/smart-contracts/testing',
+  '/developers/cosmwasm-documentation/getting-started/interact-with-contract': '/developers/getting-started/deploy-instantiate-contract',
+  '/developers/cosmwasm-documentation/getting-started/integration-overview': '/developers/smart-contracts/contract-integration',
+  '/developers/cosmwasm-documentation/getting-started/next-steps': '/developers/getting-started/interact-with-contract',
+
+  '/developers/cosmwasm-documentation/introduction': '/developers/smart-contracts/introduction',
+
+  '/developers/cosmwasm-documentation/architecture/multi-chain-contracts': '/developers/smart-contracts/architecture/multi-chain-contracts',
+  '/developers/cosmwasm-documentation/architecture/actor-model-intro': '/developers/smart-contracts/architecture/actor-model-intro',
+  '/developers/cosmwasm-documentation/architecture/names-and-addresses': '/developers/smart-contracts/architecture/names-and-addresses',
+  '/developers/cosmwasm-documentation/architecture/querying': '/developers/smart-contracts/architecture/querying',
+  '/developers/cosmwasm-documentation/architecture/serialization': '/developers/smart-contracts/architecture/serialization',
+  '/developers/cosmwasm-documentation/architecture/contract-composition': '/developers/smart-contracts/architecture/contract-composition',
+  '/developers/cosmwasm-documentation/architecture/comparison-with-ethereum': '/developers/smart-contracts/architecture/comparison-with-ethereum',
+
+  '/developers/cosmwasm-documentation/smart-contracts/contract-semantics': '/developers/smart-contracts/contract-semantics',
+  '/developers/cosmwasm-documentation/smart-contracts/message/message': '/developers/smart-contracts/message/message',
+  '/developers/cosmwasm-documentation/smart-contracts/message/submessages': '/developers/smart-contracts/message/submessages',
+
+  '/developers/cosmwasm-documentation/smart-contracts/state/simple-state': '/developers/smart-contracts/state/simple-state',
+  '/developers/cosmwasm-documentation/smart-contracts/state/complex-state': '/developers/smart-contracts/state/complex-state',
+
+  '/developers/cosmwasm-documentation/smart-contracts/result-and-option': '/developers/smart-contracts/result-and-option',
+  '/developers/cosmwasm-documentation/smart-contracts/entry-points': '/developers/smart-contracts/entry-points',
+  '/developers/cosmwasm-documentation/smart-contracts/query': '/developers/smart-contracts/query',
+  '/developers/cosmwasm-documentation/smart-contracts/events': '/developers/smart-contracts/events',
+  '/developers/cosmwasm-documentation/smart-contracts/math': '/developers/smart-contracts/math',
+  '/developers/cosmwasm-documentation/smart-contracts/verify': '/developers/smart-contracts/verify',
+  '/developers/cosmwasm-documentation/smart-contracts/migration': '/developers/smart-contracts/migration',
+  '/developers/cosmwasm-documentation/smart-contracts/migrate-dapp': '/developers/smart-contracts/migrate-dapp',
+  '/developers/cosmwasm-documentation/smart-contracts/testing': '/developers/smart-contracts/testing',
+  '/developers/cosmwasm-documentation/smart-contracts/sudo-execution': '/developers/smart-contracts/sudo-execution',
+  '/developers/cosmwasm-documentation/smart-contracts/cosmwasm-ibc': '/developers/smart-contracts/cosmwasm-ibc',
+  
+  '/developers/cosmwasm-documentation/tutorials/simple-option/testing': '/developers/guides/guides-overview',
+
+  '/developers/cosmwasm-documentation/tutorials/storage/key-value-store': '/developers/guides/storage/key-value-store',
+  '/developers/cosmwasm-documentation/tutorials/storage/indexes': '/developers/guides/storage/indexes',
+  '/developers/cosmwasm-documentation/tutorials/storage/state-modeling': '/developers/guides/storage/state-modeling',
+  
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/math': '/developers/guides/cosmwasm-by-example/math',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/instantiation': '/developers/guides/cosmwasm-by-example/instantiation',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/timelock': '/developers/guides/cosmwasm-by-example/timelock',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/crowdfunding': '/developers/guides/cosmwasm-by-example/crowdfunding',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/responses-attributes': '/developers/guides/cosmwasm-by-example/responses-attributes',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/read-write-sate': '/developers/guides/cosmwasm-by-example/read-write-sate',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/sending-tokens': '/developers/guides/cosmwasm-by-example/sending-tokens',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/token-vault': '/developers/guides/cosmwasm-by-example/token-vault',
+  '/developers/cosmwasm-documentation/tutorials/cosmwasm-by-example/amm-product': '/developers/guides/cosmwasm-by-example/amm-product',
 };
 
 export default defineNuxtRouteMiddleware(to => {
