25502998dea30ee3d31e727ca4b97e77f5fb5e16
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2024-08-07 13:25:27 -0500

    Addition of link to the ecosystem page from the hunt section (#519)

--- data/useAboutArchwayArticles.ts
+++ data/useAboutArchwayArticles.ts
@@ -13,13 +13,13 @@ export const useAboutArchwayArticles = async (): Promise<{
 
   const articles = computed(() => {
     const fetchedArticles = (data.value || []).map(item => Article.make(item));
-
+    
     // Scavenger hunt article information
     const newArticle = Article.make({
-      id: 'scavenger-hunt',
+      _id: 'scavenger-hunt',
       title: 'Scavenger Hunt',
-      description: 'Congratulations on finding the third clue! Keyword #3 is “DEVELOPER REWARDS”. Click on this module to go to the fourth clue’s location!',
-      path: '/path-to-manual-article',
+      description: 'Congratulations on finding the third clue! Keyword #3 is “DEVELOPER REWARDS”. Click on this module to go to the fourth clue\'s location!',
+      _path: 'https://archway.io/ecosystem',
       logo: 'ScavengerHuntIcon',
     });
 
