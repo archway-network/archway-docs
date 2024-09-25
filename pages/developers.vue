3d9cba3ece902680387f6e6d5e06a0a7c9644562
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2024-09-24 20:23:45 -0500

    Addition of featured tools section

--- pages/developers.vue
+++ pages/developers.vue
@@ -3,6 +3,9 @@
     <SectionsHero />
     <SectionsCategories />
     <UiBannersBuildersCall class="mt-8" />
-    <SectionsMostPopularArticles section="developers" />
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-4">
+      <SectionsMostPopularArticles section="developers" />
+      <SectionsFeaturedTools />
+    </div>
   </div>
 </template>
